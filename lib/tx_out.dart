16c33bec301d8e6d213b828d84f894124d042f3f
huliang <hl@boquaninc.com>
2021-06-17 18:07:53 +0800

    add tx out

---
+++ lib/tx_out.dart
@@ -0,0 +1,75 @@
+/*
+ * Transaction Input
+ * =================
+ *
+ * An input to a transaction. The way you probably want to use this is through
+ * the convenient method of new TxIn(txHashBuf, txOutNum, script, nSequence) (i.e., you
+ * can leave out the scriptVi, which is computed automatically if you leave it
+ * out.)
+ */
+
+import 'package:bsv/bn.dart';
+import 'package:bsv/br.dart';
+import 'package:bsv/bw.dart';
+import 'package:bsv/script.dart';
+import 'package:bsv/var_int.dart';
+
+class TxOut {
+  BigIntX valueBn;
+  VarInt scriptVi;
+  Script script;
+
+  TxOut({
+    BigIntX valueBn,
+    VarInt scriptVi,
+    Script script,
+  }) {
+    this.valueBn = valueBn;
+    this.scriptVi = scriptVi;
+    this.script = script;
+  }
+
+  factory TxOut.fromProperties({
+    BigIntX valueBn,
+    Script script,
+  }) {
+    return new TxOut().fromProperties(
+      valueBn: valueBn,
+      script: script,
+    );
+  }
+
+  TxOut setScript(Script script) {
+    this.scriptVi = VarInt.fromNumber(script.toBuffer().length);
+    this.script = script;
+    return this;
+  }
+
+  TxOut fromProperties({
+    BigIntX valueBn,
+    Script script,
+  }) {
+    var txOut = TxOut(
+      valueBn: valueBn,
+    );
+    txOut.setScript(script);
+    return txOut;
+  }
+
+  TxOut fromBr(Br br) {
+    this.valueBn = br.readUInt64LEBn();
+    this.scriptVi = VarInt.fromNumber(br.readVarIntNum());
+    this.script = new Script().fromBuffer(br.read(this.scriptVi.toNumber()));
+    return this;
+  }
+
+  Bw toBw(Bw bw) {
+    if (bw == null) {
+      bw = new Bw();
+    }
+    bw.writeUInt64LEBn(this.valueBn);
+    bw.write(this.scriptVi.buf);
+    bw.write(this.script.toBuffer());
+    return bw;
+  }
+}
