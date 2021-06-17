b900f85f885fd4b1dadce5c81c9bf73c8edf4802
huliang <hl@boquaninc.com>
2021-06-17 17:17:07 +0800

    feat:add var-int

---
+++ lib/var_int.dart
@@ -0,0 +1,72 @@
+import 'package:bsv/bn.dart';
+import 'package:bsv/br.dart';
+import 'package:bsv/bw.dart';
+import 'package:bsv/extentsions/list.dart';
+import 'package:bsv/extentsions/string.dart';
+
+/**
+ * VarInt (a.k.a. Compact Size)
+ * ============================
+ *
+ * A varInt is a varible sized integer, and it is a format that is unique to
+ * bitcoin, and used throughout bitcoin to represent the length of binary data
+ * in a compact format that can take up as little as 1 byte or as much as 9
+ * bytes.
+ */
+
+class VarInt {
+  List<int> buf;
+
+  VarInt({List<int> buf}) {
+    this.buf = buf;
+  }
+
+  factory VarInt.fromBn(BigIntX bn) {
+    return new VarInt().fromBn(bn);
+  }
+
+  factory VarInt.fromNumber(int n) {
+    return new VarInt().fromNumber(n);
+  }
+
+  String toJSON() {
+    return this.buf.toHex();
+  }
+
+  VarInt fromJSON(String str) {
+    this.buf = str.toBuffer();
+    return this;
+  }
+
+  VarInt fromBuffer(List<int> buf) {
+    this.buf = buf;
+    return this;
+  }
+
+  VarInt fromBr(Br br) {
+    this.buf = br.readVarIntBuf();
+    return this;
+  }
+
+  VarInt fromBn(BigIntX bn) {
+    this.buf = new Bw().writeVarIntBn(bn).toBuffer();
+    return this;
+  }
+
+  VarInt fromNumber(int n) {
+    this.buf = new Bw().writeVarIntNum(n).toBuffer();
+    return this;
+  }
+
+  List<int> toBuffer() {
+    return this.buf;
+  }
+
+  BigIntX toBn() {
+    return new Br(buf: this.buf.toBuffer()).readVarIntBn();
+  }
+
+  int toNumber() {
+    return new Br(buf: this.buf.toBuffer()).readVarIntNum();
+  }
+}
---
+++ test/var_int_test.dart
@@ -0,0 +1,128 @@
+import 'package:bsv/bn.dart';
+import 'package:bsv/br.dart';
+import 'package:bsv/bw.dart';
+import 'package:bsv/var_int.dart';
+import 'package:convert/convert.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:bsv/extentsions/list.dart';
+
+void main() {
+  group('VarInt', () {
+    test('should make a new varInt', () {
+      var buf = hex.decode('00');
+      var varInt = new VarInt(buf: buf);
+
+      expect(varInt.buf.toHex(), ('00'));
+
+      varInt = new VarInt(buf: buf);
+
+      // varInts can have multiple buffer representations
+      expect(
+        VarInt.fromNumber(0).toNumber(),
+        VarInt(buf: List<int>.from([0xfd, 0, 0])).toNumber(),
+      );
+
+      expect(
+        VarInt.fromNumber(0).toBuffer().toHex() ==
+            VarInt()
+                .fromBuffer(List<int>.from([0xfd, 0, 0]))
+                .toBuffer()
+                .toHex(),
+        false,
+      );
+    });
+
+    // group('#fromObject',  () {
+    //   test('should set a buffer',  () {
+    //     var buf = Buffer.from('00', 'hex')
+    //     var varInt = new VarInt().fromObject({ buf: buf })
+    //     varInt.buf.toString('hex').should.equal('00')
+    //     varInt.fromObject({})
+    //     varInt.buf.toString('hex').should.equal('00')
+    //   })
+    // })
+
+    group('#fromJSON', () {
+      test('should set a buffer', () {
+        var buf = new Bw().writeVarIntNum(5).toBuffer();
+        var varInt = new VarInt().fromJSON(buf.toHex());
+        expect(varInt.toNumber(), 5);
+      });
+    });
+
+    group('#toJSON', () {
+      test('should return a buffer', () {
+        var buf = new Bw().writeVarIntNum(5).toBuffer();
+        var varInt = new VarInt().fromJSON(buf.toHex());
+        expect(varInt.toJSON(), '05');
+      });
+    });
+
+    group('#fromBuffer', () {
+      test('should set a buffer', () {
+        var buf = new Bw().writeVarIntNum(5).toBuffer();
+        var varInt = new VarInt().fromBuffer(buf);
+        expect(varInt.toNumber(), 5);
+      });
+    });
+
+    group('#fromBr', () {
+      test('should set a buffer reader', () {
+        var buf = new Bw().writeVarIntNum(5).toBuffer();
+        var br = new Br(buf: buf.toBuffer());
+        var varInt = new VarInt().fromBr(br);
+        expect(varInt.toNumber(), 5);
+      });
+    });
+
+    group('#fromBn', () {
+      test('should set a number', () {
+        var varInt = new VarInt().fromBn(new BigIntX.fromNum(5));
+        expect(varInt.toNumber(), 5);
+      });
+    });
+
+    group('@fromBn', () {
+      test('should set a number', () {
+        var varInt = VarInt.fromBn(new BigIntX.fromNum(5));
+        expect(varInt.toNumber(), 5);
+      });
+    });
+
+    group('#fromNumber', () {
+      test('should set a number', () {
+        var varInt = new VarInt().fromNumber(5);
+        expect(varInt.toNumber(), 5);
+      });
+    });
+
+    group('@fromNumber', () {
+      test('should set a number', () {
+        var varInt = VarInt.fromNumber(5);
+        expect(varInt.toNumber(), 5);
+      });
+    });
+
+    group('#toBuffer', () {
+      test('should return a buffer', () {
+        var buf = new Bw().writeVarIntNum(5).toBuffer();
+        var varInt = new VarInt(buf: buf);
+        expect(varInt.toBuffer().toHex(), buf.toHex());
+      });
+    });
+
+    group('#toBn', () {
+      test('should return a buffer', () {
+        var varInt = VarInt.fromNumber(5);
+        expect(varInt.toBn().toString(), BigIntX.fromNum(5).toString());
+      });
+    });
+
+    group('#toNumber', () {
+      test('should return a buffer', () {
+        var varInt = VarInt.fromNumber(5);
+        expect(varInt.toNumber(), 5);
+      });
+    });
+  });
+}
