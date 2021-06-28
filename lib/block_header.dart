c00b3284456c7d77c6b01cc20df54f6177197d0d
huliang <hl@boquaninc.com>
2021-06-28 15:10:45 +0800

    add block-header full test

---
+++ lib/block_header.dart
@@ -0,0 +1,99 @@
+import 'package:bsv/br.dart';
+import 'package:bsv/bw.dart';
+import 'package:bsv/extentsions/list.dart';
+import 'package:convert/convert.dart';
+
+// ignore: slash_for_doc_comments
+/**
+ * Block Header
+ * ============
+ *
+ * Every block contains a blockHeader. This is probably not something you will
+ * personally use, but it's here if you need it.
+ */
+
+class BlockHeader {
+  int versionBytesNum;
+  List<int> prevBlockHashBuf;
+  List<int> merkleRootBuf;
+  int time;
+  int bits;
+  int nonce;
+
+  BlockHeader({
+    this.versionBytesNum,
+    this.prevBlockHashBuf,
+    this.merkleRootBuf,
+    this.time,
+    this.bits,
+    this.nonce,
+  });
+
+  factory BlockHeader.fromHex(String str) {
+    return BlockHeader.fromHex(str);
+  }
+
+  BlockHeader fromHex(String str) {
+    return this.fromBuffer(hex.decode(str));
+  }
+
+  factory BlockHeader.fromBuffer(List<int> buf) {
+    return BlockHeader().fromBuffer(buf);
+  }
+
+  BlockHeader fromBuffer(List<int> buf) {
+    return this.fromBr(Br(buf: buf));
+  }
+
+  String toHex() {
+    return this.toBuffer().toHex();
+  }
+
+  List<int> toBuffer() {
+    return this.toBw().toBuffer();
+  }
+
+  BlockHeader fromJSON(Map json) {
+    return BlockHeader(
+        versionBytesNum: json['versionBytesNum'],
+        prevBlockHashBuf: hex.decode(json['prevBlockHashBuf']),
+        merkleRootBuf: hex.decode(json['merkleRootBuf']),
+        time: json['time'],
+        bits: json['bits'],
+        nonce: json['nonce']);
+  }
+
+  Map<String, dynamic> toJSON() {
+    return {
+      "versionBytesNum": this.versionBytesNum,
+      "prevBlockHashBuf": this.prevBlockHashBuf.toHex(),
+      "merkleRootBuf": this.merkleRootBuf.toHex(),
+      "time": this.time,
+      "bits": this.bits,
+      "nonce": this.nonce
+    };
+  }
+
+  BlockHeader fromBr(Br br) {
+    this.versionBytesNum = br.readUInt32LE();
+    this.prevBlockHashBuf = br.read(32);
+    this.merkleRootBuf = br.read(32);
+    this.time = br.readUInt32LE();
+    this.bits = br.readUInt32LE();
+    this.nonce = br.readUInt32LE();
+    return this;
+  }
+
+  Bw toBw([Bw bw]) {
+    if (bw == null) {
+      bw = new Bw();
+    }
+    bw.writeUInt32LE(this.versionBytesNum);
+    bw.write(this.prevBlockHashBuf);
+    bw.write(this.merkleRootBuf);
+    bw.writeUInt32LE(this.time);
+    bw.writeUInt32LE(this.bits);
+    bw.writeUInt32LE(this.nonce);
+    return bw;
+  }
+}
---
+++ test/block_header_test.dart
@@ -0,0 +1,130 @@
+import 'package:bsv/block_header.dart';
+import 'package:bsv/br.dart';
+import 'package:bsv/bw.dart';
+import 'package:convert/convert.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:bsv/extentsions/list.dart';
+
+void main() {
+  group('BlockHeader', () {
+    var bh = new BlockHeader();
+    var versionBytesNum = 1;
+    var prevBlockHashBuf = List.generate(32, (index) => 5);
+    // prevBlockHashBuf.fill(5)
+    var merkleRootBuf = List.generate(32, (index) => 9);
+    // merkleRootBuf.fill(9)
+    var time = 2;
+    var bits = 3;
+    var nonce = 4;
+
+    bh = BlockHeader(
+        versionBytesNum: versionBytesNum,
+        prevBlockHashBuf: prevBlockHashBuf,
+        merkleRootBuf: merkleRootBuf,
+        time: time,
+        bits: bits,
+        nonce: nonce);
+    var bhhex =
+        '0100000005050505050505050505050505050505050505050505050505050505050505050909090909090909090909090909090909090909090909090909090909090909020000000300000004000000';
+    var bhbuf = hex.decode(bhhex);
+
+    // test('should make a new blockHeader', () {
+    //   var blockHeader = new BlockHeader()
+    //   should.exist(blockHeader)
+    //   blockHeader = new BlockHeader()
+    //   should.exist(blockHeader)
+    // })
+
+    group('#fromObject', () {
+      test('should set all the variables', () {
+        // bh.fromObject({
+        //   versionBytesNum: versionBytesNum,
+        //   prevBlockHashBuf: prevBlockHashBuf,
+        //   merkleRootBuf: merkleRootBuf,
+        //   time: time,
+        //   bits: bits,
+        //   nonce: nonce
+        // })
+        expect(bh.versionBytesNum != null, true);
+        expect(bh.prevBlockHashBuf != null, true);
+        expect(bh.merkleRootBuf != null, true);
+        expect(bh.time != null, true);
+        expect(bh.bits != null, true);
+        expect(bh.nonce != null, true);
+      });
+    });
+
+    group('#fromJSON', () {
+      test('should set all the variables', () {
+        var bh = new BlockHeader().fromJSON({
+          "versionBytesNum": versionBytesNum,
+          "prevBlockHashBuf": prevBlockHashBuf.toHex(),
+          "merkleRootBuf": merkleRootBuf.toHex(),
+          "time": time,
+          "bits": bits,
+          "nonce": nonce
+        });
+        expect(bh.versionBytesNum != null, true);
+        expect(bh.prevBlockHashBuf != null, true);
+        expect(bh.merkleRootBuf != null, true);
+        expect(bh.time != null, true);
+        expect(bh.bits != null, true);
+        expect(bh.nonce != null, true);
+      });
+    });
+
+    group('#toJSON', () {
+      test('should set all the variables', () {
+        var json = bh.toJSON();
+        expect(json['versionBytesNum'] != null, true);
+        expect(json['prevBlockHashBuf'] != null, true);
+        expect(json['merkleRootBuf'] != null, true);
+        expect(json['time'] != null, true);
+        expect(json['bits'] != null, true);
+        expect(json['nonce'] != null, true);
+      });
+    });
+
+    group('#fromHex', () {
+      test('should parse this known hex string', () {
+        expect(new BlockHeader().fromHex(bhhex).toBuffer().toHex(), bhhex);
+      });
+    });
+
+    group('#fromBuffer', () {
+      test('should parse this known buffer', () {
+        expect(new BlockHeader().fromBuffer(bhbuf).toBuffer().toHex(), bhhex);
+      });
+    });
+
+    group('#fromBr', () {
+      test('should parse this known buffer', () {
+        expect(new BlockHeader().fromBr(new Br(buf: bhbuf)).toBuffer().toHex(),
+            bhhex);
+      });
+    });
+
+    group('#toHex', () {
+      test('should output this known hex string', () {
+        expect(new BlockHeader().fromBuffer(bhbuf).toHex(), bhhex);
+      });
+    });
+
+    group('#toBuffer', () {
+      test('should output this known buffer', () {
+        expect(new BlockHeader().fromBuffer(bhbuf).toBuffer().toHex(), bhhex);
+      });
+    });
+
+    group('#toBw', () {
+      test('should output this known buffer', () {
+        expect(new BlockHeader().fromBuffer(bhbuf).toBw().toBuffer().toHex(),
+            bhhex);
+
+        var bw = new Bw();
+        new BlockHeader().fromBuffer(bhbuf).toBw(bw);
+        expect(bw.toBuffer().toHex(), bhhex);
+      });
+    });
+  });
+}
